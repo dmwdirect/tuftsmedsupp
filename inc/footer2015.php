96daa9957df9fa800266050639a705a8af270cde
dmwdirect <steve@dakota-diesel.com>
2015-04-08 16:04:17 -0400

    Footer Changes

--- inc/footer2015.php
+++ inc/footer2015.php
@@ -1,19 +1,12 @@
 <div class="bg_grey footer">
 <div class="container">
-<div class="pull-right component">
-<h3>Find A Meeting Near You</h3>
-<ul>
-<li class="microphone">Learn about Medigap plans at an informational meeting.<br />
-<a href="http://www.tuftsmedicarepreferred.org/sales-meetings" target="_blank" class="btn">go</a></li>
-</ul>
-</div>
-<div class="left2col">
-<p>Tufts Medicare Preferred Supplement plans are offered in accordance with Massachusetts law. Members must have Medicare Part A and Part B to enroll in this plan.&nbsp;Contact Tufts Health Plan Medicare Preferred for more information.</p>
-<p>27-MSMicrosite-15</p>
+
+
+<p>Tufts Medicare Preferred Supplement plans are offered in accordance with Massachusetts law. Members must have Medicare Part A and Part B to enroll in this plan.<br>Contact Tufts Health Plan Medicare Preferred for more information.</p>
+<p>49-MSMicrosite-15</p>
 <p><a href="http://tuftsmedicarepreferred.org">Tufts Health Plan Medicare Preferred Website</a><br>
 <div class="external_message">By clicking on this link, you will be leaving the Tufts Health Plan Medicare Preferred Supplement Plan Website.</div></p>
 
-</div>
 
 </div>
 </div>
