6f93d06f574dc42c1e67b1b31c5a5105cfbc6952
dmwdirect <digital@dmwdirect.com>
2015-04-08 15:51:01 -0400

    Create box_hmo.php

---
+++ inc/box_hmo.php
@@ -0,0 +1,7 @@
+<div class="shadow hmo">
+<div class="shadow_content rounded-nopad">
+<h3>Medicare Advantage HMO Plan</h3>
+<p>If a Medicare Supplement<br>plan is not right for you,<br>click below for our Medicare <br>Advantage HMO plan.</p>
+<div class="pull-left"><a href="http://www.tuftsmedicarepreferred.org/medicare-plans" target="_blank" class="btn btn-green">More <img src="img/arrow.png"></a></div>
+</div>
+</div>
