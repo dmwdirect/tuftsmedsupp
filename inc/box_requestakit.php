a7edc7afa459ecf157f59abe6e8779b9509aeaef
dmwdirect <digital@dmwdirect.com>
2015-04-28 17:03:58 -0400

    Update box_requestakit.php

--- inc/box_requestakit.php
+++ inc/box_requestakit.php
@@ -1,10 +1,7 @@
 <div class="shadow requestakit">
 <div class="shadow_content rounded-nopad">
-<h3>request <br />
-  a kit</h3>
-<p>Doing your Medigap <br />
-  homework? Get printed<br />
-information mailed to you.</p>
-<div class="pull-left"><a href="request-a-kit.php" class="btn btn-turq">More <img src="img/arrow.png"></a></div>
+<h3>meet</h3>
+<p>Meet with Medicare<br>Specialists in your area<br>to learn about plans that<br>may fit your health needs<br>and budget.</p>
+<div class="pull-left"><a href="http://www.tuftsmedicarepreferred.org/sales-meetings" target="_blank" class="btn btn-turq">More <img src="img/arrow.png"></a></div>
 </div>
 </div>
